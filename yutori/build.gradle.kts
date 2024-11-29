be90d350677442ef49f0cf6aa857b87b786fe25e
Yurn <ChenDSakura@163.com>
2024-11-29 20:34:54 +0800

    忘记降 Android 了()

--- yutori/build.gradle.kts
+++ yutori/build.gradle.kts
@@ -88,8 +88,8 @@ android {
         minSdk = 24
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
